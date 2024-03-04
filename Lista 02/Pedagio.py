7a24c7634a7157d337ffa8a4632046b9355a38c8
Sara_Xavier <saraaxavierrr@gmail.com>
2024-03-04 16:43:05 -0300

    ultimos exercicios da lista 02

---
+++ Lista 02/Pedagio.py
@@ -0,0 +1,8 @@
+L, D = map(int,input().split())
+K, P = map(int,input().split())
+D = L // D
+a1 = K * L
+a2 = P * D
+
+calculo = (a1 + a2)
+print(calculo)
---
+++ Lista 02/Piso_da_escola.py
@@ -0,0 +1,9 @@
+L = int(input())
+C = int(input())
+ 
+Lajota1 = (C * L) + (C - 1) * (L - 1)
+Lajota2 = (C - 1) * 2 + (L - 1) * 2
+ 
+ 
+print(int(Lajota1))
+print(int(Lajota2))
