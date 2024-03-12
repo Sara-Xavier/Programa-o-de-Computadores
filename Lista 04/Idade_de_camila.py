d6f4dbcebf1db6ca455c62c6246ecf6321d15f15
Sara-Xavier <saraaxavierrr@gmail.com>
2024-03-12 10:36:25 -0300

    Idade de camila resolvido

---
+++ Lista 04/Idade_de_camila.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+ 
+if a >= b and b >= c or c >= b and b >= a: 
+    print(b)
+elif b >= a and a >= c or c >= a and a >= b:
+    print(a)
+elif a >= c and c >= b or b >= c and c >= a:
+    print(c)
