1a14d370bf6b258b870eed9a88f655daaa592f47
Sara_Xavier <saraaxavierrr@gmail.com>
2024-03-18 18:30:39 -0300

    Exercicio concluído

---
+++ A_idade_de_dona_Monica.py
@@ -0,0 +1,8 @@
+M = int(input()) #idade dona Mônica
+A = int(input()) #idade filho 1
+B = int(input()) #idade filho 2
+C = int          #idade filho mais velho
+
+C = M - A - B
+mais_velho = (max(A,B,C))
+print(mais_velho)
