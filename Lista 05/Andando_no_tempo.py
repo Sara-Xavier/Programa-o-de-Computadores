0f9dbf65cc4e345248e76164ac3b8a62a158e1f5
Sara_Xavier <saraaxavierrr@gmail.com>
2024-03-21 19:52:59 -0300

    save

--- Lista 05/Andando_no_tempo.py
+++ Lista 05/Andando_no_tempo.py
@@ -1,4 +1,5 @@
 creditos = list(map(int, input().split()))
+
 #condições para viajar para o presente:
 if (creditos[0] + creditos[2] == creditos[1])  or (creditos[1] + creditos[2] == creditos[0]) or (creditos[0] + creditos[1] == creditos[2]) or (creditos[0] - creditos[2] == 0) or (creditos[0] - creditos[1] == 0) or (creditos[1] - creditos[2] == 0):
     print('S')
