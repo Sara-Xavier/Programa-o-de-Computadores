d0657ecb75005545298b8e29594dbd8527f15112
Sara Xavier Pires Lucena da Silva <20211014040055@ifrn.local>
2024-03-25 09:17:37 -0300

    Exercicio corrigido

--- Lista 05/Andando_no_tempo.py
+++ Lista 05/Andando_no_tempo.py
@@ -1,22 +1,13 @@
 a, b, c = map(int, input().split())
-# 0 1 2
 #condições para viajar para o presente:
 if (a + c == b) or \
    (b + c == a) or \
    (a + b == c) or \
-   (a - c == 0) or \
-   (a - b == 0) or \
-   (b - c == 0):
+   (a == b)     or \
+   (a == c)     or \
+   (c == b):
     print('S')
 
-elif (a > b + c) or \
-     (b > a + c) or \
-     (c > b + a) or \
-     (c > a + b) or \
-     (a + b + c == 0):
-      print("N")
-      
-elif (a == 0 and b == 0) or \
-     (a == 0 and c == 0) or \
-     (b == 0 and c == 0):
-        print('N')
+else: 
+   print('N')
+
